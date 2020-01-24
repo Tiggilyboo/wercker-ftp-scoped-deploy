5c1fa31c8013c61572b3d319414f5a96949d3d15
Simon Willshire <me@simonwillshire.com>
2020-01-25 11:32:16 +1300

    Initial commit, added scoped include

---
+++ README.md
@@ -0,0 +1,63 @@
+# ftp-scoped-deploy
+
+Deploy your files with FTP. It looks for list of files in the specified directory and their md5sum in **remote-file** and compare with local generated files. Any change of local files will be uploaded in this order:
+1) Add all new files
+2) Update all modified files
+3) Delete all deleted files
+
+If the order is different, active visitors of the website can see 404 pages because for example a modified file has an added menu item to a page which is not yet uploaded.
+Another case is that a file is deleted before the files referring to it are updated.
+
+# Options
+
+* `destination` (required) Full FTP path to upload to. Should start with ftp:// and end with wwwroot or public_html
+* `username` (required) Username to connect to FTP server.
+* `password` (required) Password to connect to FTP server
+* `remote-file` (optional, default is a *remote.txt*) It is a list of md5sum and filename (one filename in one row). It is should be kept synchronized with files. If it loses synchronization, simple remove all files from destination and they will be uploaded again and *remote.txt* regenerated.
+* `timeout` (optional, default is 20) Since uploading large number of files may take a long time you can define TIMEOUT when to stop before wercker stops the script. 
+* `include` (optional, default is ".") Will change directory from the pwd, and only updload from the include directory.
+
+# Example
+
+Add PASSWORD as protected environment variable. Other options can be hardcoded.
+
+```yaml
+deploy:
+  steps:
+    - duleorlovic/ftp-deploy:
+        destination: ftp://domain.example.com/site/public_html
+        username: ftpusername
+        password: $PASSWORD
+        include: public_html
+```
+
+# License
+
+The MIT License (MIT)
+
+Copyright (c) 2013 wercker
+With portions Copyright (c) 2014 duleorlovic
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+# Changelog
+
+
+## 0.0.1
+
+- Initial release
---
+++ run.sh
@@ -0,0 +1,136 @@
+#!/bin/bash
+
+# TODO if destination url does not exist, maybe we should create it
+# TODO filenames with space
+
+# curl adding is done with --ftp-create-dirs -T "$file_name" 
+# curl removing is done with -Q "-DELE $file_name" 
+# src: http://curl.haxx.se/mail/archive-2009-01/0040.html
+# these commands return an error if they fail
+
+# confirm environment variables
+if [ ! -n "$WERCKER_FTP_DEPLOY_DESTINATION" ]
+then
+    fail "missing option \"destination\", aborting"
+fi
+if [ ! -n "$WERCKER_FTP_DEPLOY_USERNAME" ]
+then
+    fail "missing option \"username\", aborting"
+fi
+if [ ! -n "$WERCKER_FTP_DEPLOY_PASSWORD" ]
+then
+    fail "missing option \"password\", aborting"
+fi
+if [ ! -n "$WERCKER_FTP_DEPLOY_SOURCE" ]
+then
+    fail "missing option \"include\", aborting"
+fi
+
+DESTINATION=$WERCKER_FTP_DEPLOY_DESTINATION
+USERNAME=$WERCKER_FTP_DEPLOY_USERNAME
+PASSWORD=$WERCKER_FTP_DEPLOY_PASSWORD
+REMOTE_FILE=$WERCKER_FTP_DEPLOY_REMOTE_FILE
+INCLUDE_DIR=$WERCKER_FTP_DEPLOY_SOURCE
+EXECUTING_DIR=$(pwd)
+
+if [ ! -n "$WERCKER_FTP_DEPLOY_REMOTE_FILE" ]
+then
+    debug "missing option \"remote-file\" so we will use remote.txt"
+    REMOTE_FILE=remote.txt
+fi
+
+# since wercker in beta allows max 25 minuter per build 
+# upload of large number of files can be separated
+TIMEOUT=20
+date_start=$(date +"%s")
+if [  -n "$WERCKER_FTP_DEPLOY_TIMEOUT" ]
+then
+    TIMEOUT=$WERCKER_FTP_DEPLOY_TIMEOUT
+fi
+debug "TIMEOUT is set to $TIMEOUT min. After that you should run this script again to complete all files. If wercker stops this script before TIMEOUT then it may happen that $REMOTE_FILE is not uploaded, so use short TIMEOUT (less than 25min)."
+
+debug "Test connection and list $DESTINATION files"
+echo "curl -u $USERNAME:do_not_show_PASSWORD_in_log $DESTINATION/"
+curl -u $USERNAME:$PASSWORD $DESTINATION/
+
+debug "Calculating md5sum for local files" 
+cd $INCLUDE_DIR
+find . -type f -exec md5sum {} > $WERCKER_CACHE_DIR/local.txt \;
+sort -k 2 -u $WERCKER_CACHE_DIR/local.txt -o $WERCKER_CACHE_DIR/local.txt > /dev/null
+
+debug "Look for $DESTINATION/$REMOTE_FILE"
+curl -u $USERNAME:$PASSWORD  $DESTINATION/$REMOTE_FILE -o $WERCKER_CACHE_DIR/remote.txt || (debug "No $REMOTE_FILE file" && echo "" > $WERCKER_CACHE_DIR/remote.txt )
+sort -k 2 -u $WERCKER_CACHE_DIR/remote.txt -o $WERCKER_CACHE_DIR/remote.txt > /dev/null
+
+debug "Find files that are new"
+cut -d' ' -f3 $WERCKER_CACHE_DIR/remote.txt > $WERCKER_CACHE_DIR/remote_files.txt
+cut -d' ' -f3 $WERCKER_CACHE_DIR/local.txt > $WERCKER_CACHE_DIR/local_files.txt
+diff --ignore-case -b --ignore-blank-lines  --old-line-format='' --new-line-format='%l
+' --unchanged-line-format=''  $WERCKER_CACHE_DIR/remote_files.txt  $WERCKER_CACHE_DIR/local_files.txt | tee $WERCKER_CACHE_DIR/new.txt > /dev/null
+sed -i '/^$/d' $WERCKER_CACHE_DIR/new.txt
+wc -l < $WERCKER_CACHE_DIR/new.txt
+
+debug "Find removed files"
+diff --ignore-case -b --ignore-blank-lines  --old-line-format='%l
+' --new-line-format='' --unchanged-line-format=''  $WERCKER_CACHE_DIR/remote_files.txt $WERCKER_CACHE_DIR/local_files.txt | tee $WERCKER_CACHE_DIR/removed.txt > /dev/null
+sed -i '/^$/d' $WERCKER_CACHE_DIR/removed.txt
+wc -l < $WERCKER_CACHE_DIR/removed.txt
+
+debug "Find changed files"
+grep -v -f $WERCKER_CACHE_DIR/new.txt $WERCKER_CACHE_DIR/local.txt | tee $WERCKER_CACHE_DIR/same_local.txt > /dev/null
+grep -v -f $WERCKER_CACHE_DIR/removed.txt $WERCKER_CACHE_DIR/remote.txt | tee $WERCKER_CACHE_DIR/same_remote.txt > /dev/null
+diff --ignore-case -b --ignore-blank-lines  --old-line-format='' --new-line-format='
+%l' --unchanged-line-format=''  $WERCKER_CACHE_DIR/same_remote.txt $WERCKER_CACHE_DIR/same_local.txt | awk '{print $2}' | tee $WERCKER_CACHE_DIR/changed.txt > /dev/null
+sed -i '/^$/d' $WERCKER_CACHE_DIR/changed.txt
+wc -l < $WERCKER_CACHE_DIR/changed.txt
+
+
+debug "Start uploading new files"
+while read file_name; do
+  if [ !  -n "$file_name" ];
+  then
+    fail "$file_name should exists"
+  else
+    echo $file_name
+    curl -u $USERNAME:$PASSWORD --ftp-create-dirs -T "$file_name" "$DESTINATION/$file_name" || fail "failed to push $file_name Please try again"
+    md5sum $file_name >> $WERCKER_CACHE_DIR/remote.txt
+    curl -u $USERNAME:$PASSWORD --ftp-create-dirs -T "$WERCKER_CACHE_DIR/remote.txt" "$DESTINATION/$REMOTE_FILE" || fail "failed to push $REMOTE_FILE. It is not in sync anymore. Please remove all files from $DESTINATION and start again"
+  fi
+  if [ "$TIMEOUT" -le $(( ($(date +"%s") - $date_start) / 60 )) ];
+  then
+    fail "TIMEOUT $TIMEOUT min has expired. Please run again this script to finish all your files."
+  fi
+done < $WERCKER_CACHE_DIR/new.txt
+
+debug "Start uploading changed files"
+while read file_name; do
+  if [ !  -n "$file_name" ];
+  then
+    fail "$file_name should exists"
+  else
+    echo $file_name
+    curl -u $USERNAME:$PASSWORD --ftp-create-dirs -T "$file_name" "$DESTINATION/$file_name" || fail "failed to push $file_name that probaably exists on server. Please try again."
+    sed -i "\|\s$file_name$|d" $WERCKER_CACHE_DIR/remote.txt 
+    md5sum $file_name >> $WERCKER_CACHE_DIR/remote.txt
+    curl -u $USERNAME:$PASSWORD --ftp-create-dirs -T "$WERCKER_CACHE_DIR/remote.txt" "$DESTINATION/$REMOTE_FILE" || fail "failed to push $REMOTE_FILE. It is not in sync anymore. Please remove all files from $DESTINATION and start again"
+  fi
+  if [ "$TIMEOUT" -le $(( ($(date +"%s") - $date_start) / 60 )) ];
+  then
+    fail "TIMEOUT $TIMEOUT min has expired. Please run again this script to finish all your files."
+  fi
+done < $WERCKER_CACHE_DIR/changed.txt
+
+debug "Start removing files"
+while read file_name; do
+  echo $file_name
+  curl -u $USERNAME:$PASSWORD -Q "-DELE $file_name" $DESTINATION/ > /dev/null || fail "$file_name does not exists on server. Please make sure your $REMOTE_FILE is synchronized."
+  sed -i "\|\s$file_name$|d" $WERCKER_CACHE_DIR/remote.txt 
+  curl -u $USERNAME:$PASSWORD --ftp-create-dirs -T "$WERCKER_CACHE_DIR/remote.txt" "$DESTINATION/$REMOTE_FILE" || fail "failed to push $REMOTE_FILE. It is not in sync anymore. Please remove all files from $DESTINATION and start again"
+done < $WERCKER_CACHE_DIR/removed.txt
+
+debug "Changing back to pwd"
+cd $EXECUTING_DIR
+
+success "Done."
+
+
